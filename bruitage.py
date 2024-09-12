fdf844d7d32fd536ccc9ce2cf0087a1e499c134d
CaNaRdEoS <125059936+CaNaRdEoS@users.noreply.github.com>
2024-09-12 12:05:31 +0200

    SNR kaka

--- bruitage.py
+++ bruitage.py
@@ -36,15 +36,27 @@ def bruitage_sel_poivre(image, taux):
                 
     return image_filtrer
 
-def puissance(image):
+def puissance_signal(image):
     puissance_image = 0
     for i in range(len(image)):
-        for j in range(len(image)):
+        for j in range(len(image[0])):
             puissance_image += image[i][j]**2
     return puissance_image
 
+def puissance_bruit(signal, bruit):
+    puissance_image = 0
+    for i in range(len(signal)):
+        for j in range(len(signal[0])):
+            if (bruit[i][j] <= signal[i][j]):
+                puissance_image += (signal[i][j]-bruit[i][j])**2
+            else:
+                puissance_image += (bruit[i][j]-signal[i][j])**2
+
+            
+    return puissance_image
+
 def SNR (signal, bruit):
-    return 10*np.log10(puissance(signal)/puissance(bruit))
+    return 10*np.log10(puissance_signal(signal)/puissance_bruit(signal, bruit))
 
 def display_image(image, titre):
     skimage.io.imshow(image, cmap="gray")
@@ -52,18 +64,26 @@ def display_image(image, titre):
     plt.show()
     print("La puissance de l'image ",titre," est de : ",puissance(image))
 
-image = skimage.io.imread(fname='image.png')
+image = skimage.io.imread(fname='./images_reference/image1_reference.png')
 display_image(image, "Image Originale")
 
 image_sel_poivre = bruitage_sel_poivre(image, 0.1)
 display_image(image_sel_poivre, "Bruitage Sel et Poivre")
-print()
-
 print("Signal sur bruit : ",SNR(image, image_sel_poivre))
-
 
 image_additif = bruitage_additif(image, 10)
 display_image(image_additif, "Buitage Additif")
+print("Signal sur bruit : ",SNR(image, image_additif))
 
 image_multipli = bruitage_multiplicatif(image, 0.2)
 display_image(image_multipli, "Buitage Multiplicatif")
+print("Signal sur bruit : ",SNR(image, image_multipli))
+
+
+#Tests de SNR
+bruit9  = skimage.io.imread(fname='./images_reference/image1_bruitee_snr_9.2885.png')
+bruit41 = skimage.io.imread(fname='./images_reference/image1_bruitee_snr_41.8939.png')
+bruit36 = skimage.io.imread(fname='./images_reference/image1_bruitee_snr_36.1414.png')
+print(np.around(SNR(image,bruit9)))
+print(np.around(SNR(image,bruit41)))
+print(np.around(SNR(image,bruit36)))
