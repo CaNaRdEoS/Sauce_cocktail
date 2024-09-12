4a0e3a414feaa600fca3e50997a3c1cf1352d234
RomainG1704 <romain-g17@hotmail.fr>
2024-09-12 10:39:26 +0200

    Bruitage sel et  poivre + caca

--- bruitage.py
+++ bruitage.py
@@ -7,17 +7,49 @@ Created on Fri Sep  6 11:14:49 2024
 
 import numpy as np
 import matplotlib.pyplot as plt
-import skimage.filters
-from skimage.util import random_noise 
+import skimage
+import random
 
-def bruitage_additive(image, taux):
-    return 0
+
+def bruitage_additif(image, taux):
+    bruit = np.random.normal(0, taux, image.shape)
+    bruitage = image + bruit
+    return bruitage
+
+def bruitage_multiplicatif(image, taux):
+    bruit = np.random.normal(0, taux, image.shape)
+    bruitage = image + (image * bruit)
+    return bruitage
 
 def bruitage_sel_poivre(image, taux):
-    image_filtrer = random_noise(image, mode='s&p', amount=taux)
+    image_filtrer = np.zeros((len(image), len(image)))
+    for i in range(len(image)) :
+        for j in range(len(image[i])) :
+            if random.randint(0, 100) <= taux * 100 :
+                if random.randint(1, 2) == 1:
+                    image_filtrer[i][j] = 255
+                
+            else :
+                image_filtrer[i][j] = image[i][j]
+                
     return image_filtrer
 
 
+def display_images(image, titre):
+    skimage.io.imshow(image, cmap="gray")
+    plt.title(titre)
+    plt.show()
+
+
 image = skimage.io.imread(fname='image.png')
+display_images(image, "Image Originale")
+
 image_sel_poivre = bruitage_sel_poivre(image, 0.1)
-skimage.io.imshow(image_sel_poivre)
+display_images(image_sel_poivre, "Buitage Sel et Poivre")
+
+
+image_additif = bruitage_additif(image, 0.1)
+display_images(image_additif, "Buitage Additif")
+
+image_multipli = bruitage_multiplicatif(image, 0.01)
+display_images(image_multipli, "Buitage Multiplicatif")
