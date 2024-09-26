79644dce2fef9a599b0b66f15b045d41da2208c2
CaNaRdEoS <wetterliam@gmail.com>
2024-09-26 11:52:28 +0200

    Convo m'énerve

--- main.py
+++ main.py
@@ -16,11 +16,12 @@ image = sk.io.imread(fname='./images_reference/image1_reference.png')
 image = image.astype(np.float64)
 
 kernel = [[1,1,1],
-        [1,1,1],
-        [1,1,1]]
+          [1,1,1],
+          [1,1,1]]
 
-###############################################################################
-# SEL POIVRE
+            #################
+            #   Sel Poivre  #
+            #################
 
 images_sel_poivre = []
 
@@ -29,30 +30,33 @@ for taux in range(1, 8,2):
     images_sel_poivre.append(image_sel_poivre)
     display_image(image_sel_poivre, "Bruitage Sel et Poivre Taux {}".format(taux/10))
 
-convolution = debruitage.debruitage_convolution(images_sel_poivre[0], kernel)
-print(np.around(snr.SNR(image,convolution)))
-display_image(convolution, "Debruitage Convolution Sel et Poivre")
-
-'''
-snr_median =[]
-snr_convolution =[]
+medians = []
+convolutions = []
+snr_median = []
+snr_convolution = []
 
 for i in range(0,len(images_sel_poivre)):
     median = debruitage.debruitage_filtre_median(images_sel_poivre[i])
     snr_median.append(np.around(snr.SNR(image,median)))
+    medians.append(median)
+    
     convolution = debruitage.debruitage_convolution(images_sel_poivre[i], kernel)
     snr_convolution.append(np.around(snr.SNR(image,convolution)))
+    convolutions.append(convolution)
+
+print("Convo 0 :",snr.SNR(image,convolutions[0]))
+print("Convo 1 :",snr.SNR(image,convolutions[1]))
 
 graph.SNR_sur_bruitage([0.1,0.3,0.5,0.7],snr_median,snr_convolution)
 
-
 # Affichage des meilleurs cas
-display_image(median, "Debruitage Sel et Poivre")
-display_image(convolution, "Debruitage Convolution Sel et Poivre")
 
 
-###############################################################################
-# Additif
+'''
+            #################
+            #    Additif    #
+            #################
+
 image_additif = bruitage.bruitage_additif(image, 30)
 display_image(image_additif, "Buitage Additif")
 
@@ -62,8 +66,10 @@ display_image(debruitage_additif, "Debruitage Additif Median")
 convolution_additif = debruitage.debruitage_convolution(image_additif, kernel)
 display_image(convolution_additif, "Debruitage Convolution Additif")
 
-###############################################################################
-# Multiplicatif
+            #################
+            # Multiplicatif #
+            #################
+
 image_multipli = bruitage.bruitage_multiplicatif(image, 1)
 display_image(image_multipli, "Buitage Multiplicatif")
 
@@ -73,17 +79,28 @@ display_image(debruitage_multipli, "Debruitage Multiplicatif Median")
 convolution_multipli = debruitage.debruitage_convolution(image_multipli, kernel)
 display_image(convolution_multipli, "Debruitage Convolution Multiplicatif")
 
-#-------------- Tests de la fonciton SNR --------------#
+            #################
+            #   Tests SNR   #
+            #################
+
 bruit9  = sk.io.imread(fname='./images_reference/image1_bruitee_snr_9.2885.png').astype(np.float64)
 bruit41 = sk.io.imread(fname='./images_reference/image1_bruitee_snr_41.8939.png').astype(np.float64)
 bruit36 = sk.io.imread(fname='./images_reference/image1_bruitee_snr_36.1414.png').astype(np.float64)
+bruit32  = sk.io.imread(fname='./images_reference/image1_bruitee_snr_32.6777.png').astype(np.float64)
+bruit28 = sk.io.imread(fname='./images_reference/image1_bruitee_snr_28.2378.png').astype(np.float64)
+bruit22 = sk.io.imread(fname='./images_reference/image1_bruitee_snr_22.2912.png').astype(np.float64)
+bruit16  = sk.io.imread(fname='./images_reference/image1_bruitee_snr_16.4138.png').astype(np.float64)
+bruit13 = sk.io.imread(fname='./images_reference/image1_bruitee_snr_13.0913.png').astype(np.float64)
+bruit10 = sk.io.imread(fname='./images_reference/image1_bruitee_snr_10.8656.png').astype(np.float64)
+
 print("Tests des images SNR de test")
 print(np.around(snr.SNR(image,bruit9)))
 print(np.around(snr.SNR(image,bruit41)))
 print(np.around(snr.SNR(image,bruit36)))
-
-
-
-#graph.SNR_sur_bruitage(X,Y,Z)
-
+print(np.around(snr.SNR(image,bruit32)))
+print(np.around(snr.SNR(image,bruit28)))
+print(np.around(snr.SNR(image,bruit22)))
+print(np.around(snr.SNR(image,bruit16)))
+print(np.around(snr.SNR(image,bruit13)))
+print(np.around(snr.SNR(image,bruit10)))
 '''
--- snr_lib.py
+++ snr_lib.py
@@ -11,14 +11,12 @@ def puissance_bruit(signal, bruit):
     puissance_image = 0
     for i in range(len(signal)):
         for j in range(len(signal[i])):
-            if (bruit[i][j] <= signal[i][j]):
-                puissance_image += (signal[i][j]-bruit[i][j])**2
-            else:
-                puissance_image += (bruit[i][j]-signal[i][j])**2
+            puissance_image += (signal[i][j]-bruit[i][j])**2
 
-            
     return puissance_image
 
 def SNR (signal, bruit):
+    puissance_signal_val = puissance_signal(signal)
+    puissance_bruit_val = puissance_bruit(signal, bruit)
 
-    return 10*np.log10(puissance_signal(signal)/puissance_bruit(signal, bruit))
+    return 10*np.log10(puissance_signal_val/puissance_bruit_val)
