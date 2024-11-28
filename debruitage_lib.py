3e3fa23e946b299897e5bd10489d793c63aa5193
RomainG1704 <romain-g17@hotmail.fr>
2024-11-28 11:26:03 +0100

    taille 3x3 et 5x5

--- debruitage_lib.py
+++ debruitage_lib.py
@@ -2,14 +2,24 @@ import numpy as np
 import contour_lib as contour
 from skimage import feature
 
-def debruitage_filtre_median(image):
+def debruitage_filtre_median(image, taille):
+    
+    if taille not in [3, 5]:
+        print("Kernel non implémenté")
+        exit()
+        
     debruitage = np.zeros((len(image), len(image)))
     
-    for i in range(2, len(image)-2):
-        for j in range(2, len(image[i])-2) :
-            pixels = [image[i+x][j+y] for x in range(-2, 3) for y in range(-2, 3)]
-    
-            debruitage[i][j] = np.median(pixels)
+    if taille == 3 :
+        for i in range(1, len(image)-1):
+            for j in range(1, len(image[i])-1) :
+                pixels = [image[i+x][j+y] for x in range(-1, 2) for y in range(-1, 2)]
+    else :
+        for i in range(2, len(image)-2):
+            for j in range(2, len(image[i])-2) :
+                pixels = [image[i+x][j+y] for x in range(-2, 3) for y in range(-2, 3)]
+        
+    debruitage[i][j] = np.median(pixels)
             
     return debruitage
 
